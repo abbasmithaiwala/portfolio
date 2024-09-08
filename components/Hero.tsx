c42bea1fc4ec53d8ca0fd01373fdd78734eaf529
abbas-08 <mithaiwalaabbas1@gmail.com>
2024-09-09 01:06:52 +0530

    Minor Change

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
 
                 <TextGenerateEffect 
                  className='text-center text-[40px] md:text-5xl lg:text-6xl'
-                 words='Transforming Concepts into Seamless User Experiences'
+                 words='Crafting Intelligence: One Algorithm at Time'
                 />
                 <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
                     Hi, I&apos;m Abbas Mithaiwala,
