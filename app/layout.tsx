2291d00326d4f5ed1e62a0d8bc7b2e780ddaf9de
abbasmithaiwala <mithaiwalaabbas1@gmail.com>
2024-12-22 02:30:16 +0530

    Updated index

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,17 +18,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-      <GoogleAnalytics gaId="G-ZTYWF0FBKE" />
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-      </body>
+    <body className={inter.className}>
+    <GoogleAnalytics gaId="G-ZTYWF0FBKE"/>
+    <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+    >
+        {children}
+    </ThemeProvider>
+
+    <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
+    <script src="https://files.bpcontent.cloud/2024/12/21/19/20241221192809-RY6ROYCO.js"></script>
+
+    </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,17 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
-      <div className="max-w-7xl w-full">
-        < FloatingNav navItems={navItems}/>
-        <Hero/>
-        <Grid/>
-        <RecentProjects/>
-        <Experience />
-        <Approach />
-        <Clients />
-        <Footer />
-      </div>
+      <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+        <div className="max-w-7xl w-full">
+          < FloatingNav navItems={navItems}/>
+          <Hero/>
+          <Grid/>
+          <RecentProjects/>
+          <Experience />
+          <Approach />
+          <Clients />
+          <Footer />
+        </div>
     </main>
   );
 }
